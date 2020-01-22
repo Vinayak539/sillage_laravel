5aea7d376c46c395ac74f106d0ded7260a140979
abhishekg55 <abhishekgupta5544@gmail.com>
2020-01-22 13:03:21 +0530

    subscriber bug fixxed

--- resources/views/backend/admin/subscribers/index.blade.php
+++ resources/views/backend/admin/subscribers/index.blade.php
@@ -13,44 +13,9 @@
     </nav>
 
     <div class="card">
-        <div class="card-body">
-            <form class="needs-validation" autocomplete="off" method="GET">
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="form-group">
-                        <label for="city">City</label>
-                        <input type="text" name="city" class="form-control" id="city" placeholder="Enter City"
-                            value="{{Request::get('city')}}">
-                    </div>
-                </div>
-
-                <div class="col-md-4">
-                    <div class="form-group">
-                        <label for="territory">State</label>
-                        <input type="text" name="territory" class="form-control" id="territory"
-                            value="{{Request::get('territory')}}" placeholder="Enter State">
-                    </div>
-                </div>
-
-                <div class="col-md-4">
-                    <div class="form-group">
-                        <label for="pincode">Pincode</label>
-                        <input type="number" name="pincode" class="form-control" id="pincode"
-                            value="{{Request::get('pincode')}}" placeholder="Enter Pincode" minlength="5" min="1">
-                    </div>
-                </div>
-
-                <div class="col-md-12 text-center">
-                    <button type="submit" class="btn btn-primary btnSubmit">
-                        <i class="fa fa-search"></i> Search
-                    </button>
-                </div>
-            </div>
-        </form>
+        <div class="card-header bg-dark text-white-all">
+            <h4>Manage Subscribers</h4>
         </div>
-    </div>
-
-    <div class="card">
         <div class="card-body">
             <div class="table-responsive dt-responsive">
                 <form action="{{ route('admin.subscribers.show') }}" method="POST">
@@ -58,7 +23,7 @@
                     <table class="table table-striped table-hover" style="width:100%;">
                         <thead>
                             <tr>
-                                <td colspan="6">
+                                <td colspan="4">
                                     <div class="text-right">
                                         @if(count($subscribers))
                                         <button type="submit" class="btn btn-outline-info">
@@ -74,9 +39,7 @@
                                     <label for="ckbCheckAll">All</label>
                                 </th>
                                 <th>Email</th>
-                                <th>Pincode</th>
-                                <th>City</th>
-                                <th>State</th>
+                                <th>Status</th>
                                 <th>Subscribed On</th>
                             </tr>
                         </thead>
@@ -88,14 +51,12 @@
                                         class="checkBoxClass">
                                 </td>
                                 <td>{{ $subscriber->email }}</td>
-                                <td>{{ $subscriber->pincode }}</td>
-                                <td>{{ $subscriber->city }}</td>
-                                <td>{{ $subscriber->territory }}</td>
+                                <td>{{ $subscriber->status ? 'Subscribed' : 'Unsubscribed' }}</td>
                                 <td>{{ date('d-M-Y h:i A', strtotime($subscriber->created_at)) }}</td>
                             </tr>
                             @empty
                             <tr class="text-center">
-                                <td class="text-danger" colspan="6">
+                                <td class="text-danger" colspan="4">
                                     <h4>No Subscriber Found..</h4>
                                 </td>
                             </tr>
@@ -105,13 +66,11 @@
                             <tr>
                                 <th>#</th>
                                 <th>Email</th>
-                                <th>Pincode</th>
-                                <th>City</th>
-                                <th>State</th>
+                                <th>Status</th>
                                 <th>Subscribed On</th>
                             </tr>
                             <tr>
-                                <td colspan="6">
+                                <td colspan="4">
                                     <div class="text-right">
                                         @if(count($subscribers))
                                         <button type="submit" class="btn btn-outline-info">
